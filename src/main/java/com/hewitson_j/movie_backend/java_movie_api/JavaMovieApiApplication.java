c8eb7aa8be17a731c8c425a059eb199a0bba878d
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2025-03-03 18:11:34 -0700

    Initial commit

---
+++ src/main/java/com/hewitson_j/movie_backend/java_movie_api/JavaMovieApiApplication.java
@@ -0,0 +1,14 @@
+package com.hewitson_j.movie_backend.java_movie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMovieApiApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
