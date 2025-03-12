1122c0b7565933cbaef604b33d274d9f22593a3f
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2025-03-12 14:00:46 -0600

    Updated pom.xml, ran mvn clean packages for deploy

--- src/test/java/com/hewitson_j/movie_backend/java_movie_api/JavaMovieApiApplicationTests.java
+++ src/test/java/com/hewitson_j/movie_backend/java_movie_api/JavaMovieApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.hewitson_j.movie_backend.java_movie_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JavaMovieApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class JavaMovieApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
